860b813073214fc73ba0e463ad727eccb62b96ff
Ebi manoj <ebimanoj28@gmail.com>
2025-09-24 09:51:22 +0530

    Initial commit

---
+++ Client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './index.css';
+import { Signup } from './signup';
+
+function App() {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
