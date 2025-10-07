db76cb7e802cb0432e1f4055dd278d18bee27307
Ebi manoj <ebimanoj28@gmail.com>
2025-10-07 10:40:55 +0530

    added axios interceptors

--- Client/src/utils/axiosInstance.ts
+++ Client/src/utils/axiosInstance.ts
@@ -1,3 +1,5 @@
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { logout, refreshToken } from '@/store/features/auth/auth.thunks';
 import type { Store } from '@reduxjs/toolkit';
 import axios from 'axios';
 let store: Store;
@@ -8,6 +10,7 @@ const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
+
 axiosInstance.interceptors.request.use(
   config => {
     const state = store.getState();
@@ -19,4 +22,31 @@ axiosInstance.interceptors.request.use(
   },
   error => Promise.reject(error)
 );
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const dispatch = useAppDispatch();
+      try {
+        await dispatch(refreshToken()).unwrap();
+        const state = store.getState();
+        const token = state.auth.token;
+
+        originalRequest.headers.Authorization = `Bearer ${token}`;
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        await dispatch(logout());
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
