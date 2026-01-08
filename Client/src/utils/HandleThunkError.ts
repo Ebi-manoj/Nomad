e4518adc08eeed270b39904473826cb53aa117e4
Ebi manoj <ebimanoj28@gmail.com>
2026-01-08 17:24:23 +0530

    fix:removed plan.tsx file

--- Client/src/utils/HandleThunkError.ts
+++ Client/src/utils/HandleThunkError.ts
@@ -1,14 +1,14 @@
 import { ErrorMessage } from '@/utils/constants';
 import { isAxiosError } from 'axios';
 
-export function handleThunkError<TRejectValue = string>(
+export function handleThunkError<T>(
   error: unknown,
-  rejectWithValue: (value: TRejectValue) => unknown,
+  rejectWithValue: (value: T) => T,
   defaultMsge: string = ErrorMessage.SOMETHING_WENT_WRONG
 ) {
   console.log('error object', error);
   if (isAxiosError(error)) {
     return rejectWithValue(error.response?.data?.error?.message || defaultMsge);
   }
-  return rejectWithValue(ErrorMessage.SERVER_ERROR as TRejectValue);
+  return rejectWithValue(ErrorMessage.SERVER_ERROR as T);
 }
